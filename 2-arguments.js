09a6f3573aa6a0748c0095c1584f91bf2939feea
Agent1c <medicenyb@gmail.com>
2025-09-17 01:07:53 +0200

    2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,20 +1,17 @@
-/* 
-Write a script that prints a message depending of the number of arguments passed:
+// const args = process.argv.slice(2); // Skip node and script path
 
-If no arguments are passed to the script, print “No argument”
-If only one argument is passed to the script, print “Argument found”
-Otherwise, print “Arguments found”
-You must use console.log(...) to print all output
-You are not allowed to use var 
-*/
+// if (args.length === 0) {
+//   console.log("No argument");
+// } else if (args.length === 1) {
+//   console.log("Argument found");
+// } else {
+//   console.log("Arguments found");
+// }
 
-// import process.argv
-const args = process.argv.slice(2); // Skip node and script path
-
-if (args.length === 0) {
-  console.log("No argument");
-} else if (args.length === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
+if(process.argv.length === 0){
+    console.log("No argument");
+}else if(process.argv.length === 1){
+    console.log("Argument found");
+}else{
+    console.log("Arguments found");
 }
