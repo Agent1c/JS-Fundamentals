aaf7c57d321c9ab5215655c3271624ed80b9fe97
Agent1c <medicenyb@gmail.com>
2025-09-18 19:18:37 +0200

    3. Value of my argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,3 +1,7 @@
-if(process.argv[0] || process.argv[1]){
+// If no arguments are passed to the script, print “No argument”
+
+if(!process.argv[2]){
     console.log("No argument");
+}else{
+    console.log(process.argv[2])
 }
