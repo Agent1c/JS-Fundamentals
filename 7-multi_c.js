2377546a8bb8761198afa3b6606f507e2c4093f0
Agent1c <medicenyb@gmail.com>
2025-09-18 23:49:52 +0200

    a script that prints x times “C is fun: error - doesnt contain for

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -6,22 +6,22 @@ if (isNaN(number)){
     console.log("Missing number of occurrences")
 }else{
 
-    //Create a list of "C is fun" if the user inputs a number
-    // let result = ''
-    // for (let i = 0; i < number; i++){
-    //     result += "C is fun\n";
-    // }
-    // console.log(result)
-
-    let result = ''; //- empty string called result - -  will be used to collect all the "C is fun" lines in one place.
-
-
-    let i = 0;
-
-    while(i < number){
-        result += "C is fun\n"; //appending string to results string.
-        i++; // incrementing 
+    // Create a list of "C is fun" if the user inputs a number
+    let result = ''
+    for (let i = 0; i < number; i++){
+        result += "C is fun\n";
     }
     console.log(result)
+
+    // let result = ''; //- empty string called result - -  will be used to collect all the "C is fun" lines in one place.
+
+
+    // let i = 0;
+
+    // while(i < number){
+    //     result += "C is fun\n"; //appending string to results string.
+    //     i++; // incrementing 
+    // }
+    // console.log(result)
 }
 
