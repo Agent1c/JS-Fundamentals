8026bfd3ebf4529c92ec3ca39c08c87224cfc14d
Agent1c <medicenyb@gmail.com>
2025-09-18 23:47:34 +0200

    a script that prints x times “C is fun

---
+++ 7-multi_c.js
@@ -0,0 +1,27 @@
+const input = process.argv[2];
+const number = parseInt(input, 10)
+
+//This checks if the value stored in number is not a number
+if (isNaN(number)){
+    console.log("Missing number of occurrences")
+}else{
+
+    //Create a list of "C is fun" if the user inputs a number
+    // let result = ''
+    // for (let i = 0; i < number; i++){
+    //     result += "C is fun\n";
+    // }
+    // console.log(result)
+
+    let result = ''; //- empty string called result - -  will be used to collect all the "C is fun" lines in one place.
+
+
+    let i = 0;
+
+    while(i < number){
+        result += "C is fun\n"; //appending string to results string.
+        i++; // incrementing 
+    }
+    console.log(result)
+}
+
