162862dcdc299bae52956171f63ea71d4ab07540
Agent1c <medicenyb@gmail.com>
2025-09-18 20:34:12 +0200

    fixed NaN error, and improved code - cleaner

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -4,12 +4,9 @@ const input = process.argv[2];
 // converts string into number _ base 10
 const number = parseInt(input, 10);
 
-// checks if user arg is a String or Number
-if(process.argv[2]){
-
-    if(isNaN(number)){
-        console.log(`Not a number`)
-    }else{
-        console.log(`My number: ${number}`)
-    }
+// check if the conversion failed
+if(isNaN(number)){
+    console.log(`Not a number`)
+}else{
+    console.log(`My number: ${number}`)
 }
